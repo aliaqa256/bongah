46e24c9fc1fa0d1b1788d66274a806a1257a5345
aliaqa_ubuntu <alilotfi256@gmail.com>
2022-06-30 05:17:21 -0700

    add contact to excel

--- scraper/main.py
+++ scraper/main.py
@@ -4,26 +4,30 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import requests
 
+cookies = {
+    'token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoiMDkzMzg3ODc1MTAiLCJpc3MiOiJhdXRoIiwiaWF0IjoxNjU2NTg5Nzg3LCJleHAiOjE2NTc4ODU3ODcsInZlcmlmaWVkX3RpbWUiOjE2NTY1ODk3ODcsInVzZXItdHlwZSI6InBlcnNvbmFsIiwidXNlci10eXBlLWZhIjoiXHUwNjdlXHUwNjQ2XHUwNjQ0IFx1MDYzNFx1MDYyZVx1MDYzNVx1MDZjYyIsInNpZCI6ImY5ZjRlNzM5LTI0ODEtNDc1OS05MDMwLTc0ZThiZmE4MWE0MyJ9.Bhicmnly7tESNKKUxeyMfjsSyWCdEl7SyZJfUxSSA7E',
+    # 'cookiesession1': '678ADA5AOQRSTV135688024BDFHJ9BD1',
 
-main_array = []
+}
 
 
-url_of_all_new_personal_home_in_tehran='https://divar.ir/s/tehran/real-estate?user_type=personal'
-
-# get the url with bs4
 def get_url_with_bs4(url):
-    page = requests.get(url)
+    page = requests.get(url, cookies=cookies)
     soup = BeautifulSoup(page.content, 'html.parser')
     return soup
 
-response = get_url_with_bs4(url_of_all_new_personal_home_in_tehran)
 
+url_of_all_new_personal_home_in_tehran = 'https://divar.ir/s/tehran/real-estate?user_type=personal'
+
+response = get_url_with_bs4(url_of_all_new_personal_home_in_tehran)
 
 # select all items with .kt-post-card--outlined class
 cards = response.select('.kt-post-card--outlined')
 
+main_array = []
+
 for card in cards:
-    array_of_data=[]
+    array_of_data = []
     # select the title of the card
     title = card.select('.kt-post-card__title')[0].getText()
     # select the price of the card
@@ -33,7 +37,8 @@ for card in cards:
         price = '0'
 
     # select the address of the card
-    address = card.select('.kt-post-card__bottom-description')[0].getText().split(' ')[-1]
+    address = card.select(
+        '.kt-post-card__bottom-description')[0].getText().split(' ')[-1]
 
     # select link of the card
     link = 'https://divar.ir'+card.get('href')
@@ -42,7 +47,8 @@ for card in cards:
     detail_page = get_url_with_bs4(link)
 
     try:
-        description = detail_page.select('.kt-description-row .kt-base-row__start')[0].getText()
+        description = detail_page.select(
+            '.kt-description-row .kt-base-row__start')[0].getText()
     except:
         description = ''
 
@@ -55,7 +61,7 @@ for card in cards:
             year = ''
     except:
         year = ''
-    
+
     try:
         rooms = detail_page.select(
             '.kt-group-row-item--info-row~ .kt-group-row-item--info-row+ .kt-group-row-item--info-row .kt-group-row-item__value')[0].getText()
@@ -65,9 +71,7 @@ for card in cards:
 
     except:
         rooms = ''
-    
-  
-   
+
     try:
         area = detail_page.select(
             '.kt-group-row-item--info-row:nth-child(1) .kt-group-row-item__value')[0].getText()
@@ -79,7 +83,7 @@ for card in cards:
     try:
         floor = detail_page.select(
             '.kt-unexpandable-row:nth-child(13)')[0].getText()
-        
+
         if not 'طبقه' in floor:
             floor = ''
 
@@ -87,7 +91,8 @@ for card in cards:
         floor = ''
 
     try:
-        anbari = detail_page.select('.kt-icon-cabinet+ .kt-body--stable')[0].getText()
+        anbari = detail_page.select(
+            '.kt-icon-cabinet+ .kt-body--stable')[0].getText()
         if not 'انبار' in anbari:
             anbari = ''
     except:
@@ -102,23 +107,25 @@ for card in cards:
         parking = ''
 
     try:
-        elevator = detail_page.select('.kt-icon-elevator+ .kt-body--stable')[0].getText()
+        elevator = detail_page.select(
+            '.kt-icon-elevator+ .kt-body--stable')[0].getText()
         if not 'آسانسور' in elevator:
             elevator = ''
     except:
         elevator = ''
-    
+
     try:
         price_per_meter = detail_page.select(
             '.kt-unexpandable-row:nth-child(5)')[0].getText()
         if not 'قیمت هر متر' in price_per_meter:
             price_per_meter = ''
-    
+
     except:
         price_per_meter = ''
 
     try:
-        total_price = detail_page.select('.kt-unexpandable-row:nth-child(3)')[0].getText()
+        total_price = detail_page.select(
+            '.kt-unexpandable-row:nth-child(3)')[0].getText()
         if not 'قیمت کل' in total_price:
             total_price = ''
     except:
@@ -141,7 +148,9 @@ for card in cards:
     except:
         sub_type = ''
 
-
+    token_in_the_url = link.split('/')[-1]
+    contact = requests.get(
+        f'https://api.divar.ir/v5/posts/{token_in_the_url}/contact/', cookies=cookies).json()['widgets']['contact']['phone']
 
     array_of_data.append(title)
     array_of_data.append(price)
@@ -160,32 +169,18 @@ for card in cards:
     array_of_data.append(type_of_sell)
     array_of_data.append(main_type)
     array_of_data.append(sub_type)
-    print('.','one home is done')
+    array_of_data.append(contact)
+    
+    print('.', 'one home is done')
 
     main_array.append(array_of_data)
 
 
 print('creating data frame............')
 # creaete dataframe from the main array
-df = pd.DataFrame(main_array, columns=['title', 'price', 'address', 'link', 'description', 'year', 'rooms', 'area', 'floor', 'anbari', 'parking', 'elevator', 'price_per_meter', 'total_price', 'type_of_sell', 'main_type', 'sub_type'])
+df = pd.DataFrame(main_array, columns=['title', 'price', 'address', 'link', 'description', 'year', 'rooms', 'area',
+                  'floor', 'anbari', 'parking', 'elevator', 'price_per_meter', 'total_price', 'type_of_sell', 'main_type', 'sub_type', 'contact'])
 
 print("creating excel file............")
 # create excel file from the dataframe
 df.to_excel('divar.xlsx')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
