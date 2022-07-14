f1638e8f167e403439b288088d50d89b713c5958
aliaqa <ali@ubuntu_jammy>
2022-07-14 14:53:53 +0430

    fix the task

--- djangoapp/accounts/tasks.py
+++ djangoapp/accounts/tasks.py
@@ -2,55 +2,53 @@ from celery import shared_task
 
 from .utils import divar_scraper
 
-from .models import Home,User
+from .models import Home, User
 from .robot import send_homes
+
+
 @shared_task
 def main_bongah_task():
 
-   # 1- get all new home
-   array_of_homes = divar_scraper()
-   print("all the homes are taken")
-    # 2-save the new ones (not existing)
-   for home in array_of_homes:
-      if not Home.objects.filter(title=home['title']).exists():
-            new_home=Home(**home)
+    # 1- get all new home
+    array_of_homes = divar_scraper()
+    print("all the homes are taken")
+
+    for home in array_of_homes:
+        if not Home.objects.filter(title=home['title']).exists():
+            new_home = Home(**home)
             new_home.save()
             print("one home is saved in db")
-      # 3-get all users
-      users = User.objects.all() #TODO if has paid
-      # 4-send each user the home with the keyword or neighborhood they want with telegram bot
-      for user in users:
-            # check if this homes title or address is in the user searchwords.word
-            user_searchwords = user.search_words.all()
-            # for word in user_searchwords:
-                  # if home['title'] in word.word or home['address'] in word.word:
-                  # if True:
-                        # send the home to the user
-            send_homes(
-                int(user.username), f" {home['title']} #{home['address']}  {home['description']} {home['price']}  {home['year']} {home['rooms']} {home['floor']} {home['parking']} {home['elevator']} {home['price_per_meter']}  {home['total_price']} {home['contact']}  #{home['type_of_sell']} \
-                  #{home['main_type']} #{home['sub_type']}  {home['link']} ")
 
-      return 'done'   
+        users = User.objects.filter(is_active=True, has_day_left=True)
+        for user in users:
+            user_searchwords = user.search_words.all()
+            for word in user_searchwords:
+                  if home['title'] in word.word or home['address'] in word.word:
+                        send_homes(
+                        int(user.username), f" {home['title']} \n #{home['address']} \n \
+                        {home['description']} \n{home['price']} \n {home['year']}\n  {home['rooms']} \n  \
+                              {home['floor']} \n {home['parking']} \n {home['elevator']} \n \
+                              {home['price_per_meter']}  \n {home['total_price']} \n {home['contact']}  \n #{home['type_of_sell']} \
+                              #{home['main_type']} #{home['sub_type']}  {home['link']} ")
+
+        return 'done'
 
 
 @shared_task
 def home_deleter():
-      """ delete 4 oldest home in database if exists """
-      if Home.objects.all().count() > 4:
-            homes=Home.objects.order_by('id')[:4]
-            for home in homes:
-                  home.delete()
+    """ delete 4 oldest home in database if exists """
+    if Home.objects.all().count() > 4:
+        homes = Home.objects.order_by('id')[:4]
+        for home in homes:
+            home.delete()
+
+    else:
+        return 'not done'
 
 
-      else:
-            return 'not done'
-
-
-# TODO add task for reduce the day per day
 @shared_task
 def reduceUserDaysLeft():
     users = User.objects.filter(has_day_left=True)
     for user in users:
-      user.days_left = user.days_left - 1
-      user.save()
-
+        user.days_left = user.days_left - 1
+        user.save()
---
+++ scraper/from_djago.py
@@ -0,0 +1,193 @@
+from bs4 import BeautifulSoup
+import requests
+
+cookies = {
+    'token': "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoiMDkxNDc1NzA1MjMiLCJpc3MiOiJhdXRoIiwiaWF0IjoxNjU3NzkzNTg2LCJleHAiOjE2NTkwODk1ODYsInZlcmlmaWVkX3RpbWUiOjE2NTcwMjYwOTksInVzZXItdHlwZSI6InJlYWwtZXN0YXRlLWJ1c2luZXNzIiwidXNlci10eXBlLWZhIjoiXHUwNjdlXHUwNjQ2XHUwNjQ0IFx1MDYyN1x1MDY0NVx1MDY0NFx1MDYyN1x1MDZhOSIsInNpZCI6ImExYjMwNGJlLTM0OTgtNDQ5Ni1hNjAxLWI0ZjlhOTY0MzI1YiJ9.NL1vrFwa7QUvDeXWsyBkKjCjNdiaDd2p6FNt-jop74g"
+}
+
+
+def get_url_with_bs4(url):
+    page = requests.get(url, cookies=cookies)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    return soup
+
+
+url_of_all_new_personal_home_in_tehran = 'https://divar.ir/s/tehran/real-estate?user_type=personal'
+
+
+def divar_scraper():
+    array_of_homes = []
+    response = get_url_with_bs4(url_of_all_new_personal_home_in_tehran)
+
+    cards = response.select('.kt-col-xxl-4')
+
+    for card in cards:
+        # select the title of the card
+        title = card.select('.kt-post-card__title')[0].getText()
+        # select the price of the card
+        # if home with this title is in database, skip it
+
+
+        try:
+            price = card.select('.kt-post-card__description')[0].getText()
+        except:
+            price = ''
+            print('price')
+
+        # select the address of the card
+            # select the address of the card
+
+        try:
+            address = card.select(
+                '.kt-post-card__bottom-description')[0].getText().split(' ')
+            index_of_dar = address.index('در')+1
+            # join the word that index is index_of_dar
+            address = ' '.join(address[index_of_dar:])
+        except:
+            address = ''
+            print('address')
+
+        try:
+
+            # select link of the card
+            link = 'https://divar.ir' + card.select('a')[0].get('href')
+
+            # get the page of link with bs4
+            detail_page = get_url_with_bs4(link)
+        except:
+            link = ''
+            print('link is not found')
+        try:
+            description = detail_page.select(
+                '.kt-description-row .kt-base-row__start')[0].getText()
+        except:
+            description = ''
+            print('description')
+
+        try:
+            year = detail_page.select(
+                '.kt-group-row-item--info-row:nth-child(2) .kt-group-row-item__value')[0].getText()
+            if len(year) == 4:
+                year = year
+            else:
+                year = ''
+        except:
+            year = ''
+
+        try:
+            rooms = detail_page.select(
+                '.kt-group-row-item--info-row~ .kt-group-row-item--info-row+ .kt-group-row-item--info-row .kt-group-row-item__value')[0].getText()
+
+            if int(rooms) > 5:
+                rooms = ''
+
+        except:
+            rooms = ''
+
+        try:
+            area = detail_page.select(
+                '.kt-group-row-item--info-row:nth-child(1) .kt-group-row-item__value')[0].getText()
+            if int(area) > 1300 or int(area) < 10:
+                area = ''
+        except:
+            area = ''
+
+        try:
+            floor = detail_page.select(
+                '.kt-unexpandable-row:nth-child(13)')[0].getText()
+
+            if not 'طبقه' in floor:
+                floor = ''
+
+        except:
+            floor = ''
+
+        try:
+            anbari = detail_page.select(
+                '.kt-icon-cabinet+ .kt-body--stable')[0].getText()
+            if not 'انبار' in anbari:
+                anbari = ''
+        except:
+            anbari = ''
+
+        try:
+            parking = detail_page.select(
+                '.kt-icon-parking+ .kt-body--stable')[0].getText()
+            if not 'پارکینگ' in parking:
+                parking = ''
+        except:
+            parking = ''
+
+        try:
+            elevator = detail_page.select(
+                '.kt-icon-elevator+ .kt-body--stable')[0].getText()
+            if not 'آسانسور' in elevator:
+                elevator = ''
+        except:
+            elevator = ''
+
+        try:
+            price_per_meter = detail_page.select(
+                '.kt-unexpandable-row:nth-child(5)')[0].getText()
+            if not 'قیمت هر متر' in price_per_meter:
+                price_per_meter = ''
+
+        except:
+            price_per_meter = ''
+
+        try:
+            total_price = detail_page.select(
+                '.kt-unexpandable-row:nth-child(3)')[0].getText()
+            if not 'قیمت کل' in total_price:
+                total_price = ''
+        except:
+            total_price = ''
+
+        if 'اجاره' in price or 'ودیعه' in price:
+            type_of_sell = 'rent'
+        else:
+            type_of_sell = 'sell'
+
+        try:
+            main_type = detail_page.select(
+                '.kt-breadcrumbs__item:nth-child(2) .kt-breadcrumbs__link')[0].getText()
+        except:
+            main_type = ''
+
+        try:
+            sub_type = detail_page.select(
+                '.kt-breadcrumbs__item~ .kt-breadcrumbs__item+ .kt-breadcrumbs__item a')[0].getText()
+        except:
+            sub_type = ''
+
+        token_in_the_url = link.split('/')[-1]
+        try:
+            contact = requests.get(
+                f'https://api.divar.ir/v5/posts/{token_in_the_url}/contact/', cookies=cookies).json()['widgets']['contact']['phone']
+        except:
+            contact = ''
+        home = {'title': title,
+                'price': price,
+                'address': address,
+                'link': link,
+                'description': description,
+                'year': year,
+                'rooms': rooms,
+                'area': area,
+                'floor': floor,
+                'anbari': anbari,
+                'parking': parking,
+                'elevator': elevator,
+                'price_per_meter': price_per_meter,
+                'total_price': total_price,
+                'type_of_sell': type_of_sell,
+                'main_type': main_type,
+                'sub_type': sub_type,
+                'contact': contact}
+        array_of_homes.append(home)
+        print('home is added')
+
+    return array_of_homes
+
+
+print(divar_scraper())
