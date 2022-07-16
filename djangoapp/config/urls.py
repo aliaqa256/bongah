6659d232f9711d6aab80a404be0b2181b6b2b3fb
aliaqa <ali@ubuntu_jammy>
2022-07-16 15:05:05 +0430

    static 2

--- djangoapp/config/urls.py
+++ djangoapp/config/urls.py
@@ -15,9 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('accounts.urls')),
     path('payments/',include('zarinpal.urls')),
 ]
+if settings.DEBUG == True:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
